2689aa199c0da559cd59c8a7e759d896be9be1cc
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-08-09 12:58:30 +0200

    fixing key for HistoryItem without index of map method

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,6 +4,7 @@ import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
+import CounterHistory from './CounterHistory.jsx'
 import { log } from '../../log.js';
 
 function isPrime(number) {
@@ -37,15 +38,23 @@ const Counter =  memo (function Counter({ initialCount }) {
   //altrimenti si sarebbe rieseguita ad ogni re-render di questo component
   const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  //const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([{value : initialCount, id: Math.random() * 1000}]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
 
   //grazie a useCallback prevengo che questa funzione venga ricreata ad ogni esecuzione di questo component, così memo su IconButton funzionerà visto che le prop non cambieranno
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    //setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value: -1, id: Math.random() * 1000}, ...prevCounterChanges]);
   }, [])
 
    const handleIncrement = useCallback (function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    //setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{value: 1, id: Math.random() * 1000}, ...prevCounterChanges]);
   }, [])
 
   return (
@@ -58,11 +67,12 @@ const Counter =  memo (function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 })
